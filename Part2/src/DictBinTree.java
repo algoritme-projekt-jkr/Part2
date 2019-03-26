8640f0f02883035c7de7c377db25856135f8a66e
Robinlp <Robinlp@users.noreply.github.com>
2019-03-26 15:16:04 +0100

    init

---
+++ Part2/src/DictBinTree.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author Robin
+ */
+public class DictBinTree implements Dict{
+
+    @Override
+    public void insert(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int[] orderedTraversal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean search(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
